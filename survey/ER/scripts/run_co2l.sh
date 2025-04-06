cb56ffb664cb88a5b1001b85573e2ca35067c3e2
ik <127835609+KoyoImai@users.noreply.github.com>
2025-04-06 21:19:00 +0900

    Delete survey/ER directory

--- survey/ER/scripts/run_co2l.sh
+++
@@ -1,7 +0,0 @@
-# cifar10
-# python main.py --method co2l --mem_type ring --dataset cifar10 --batch_size 512 \
-#                --current_temp 0.2 --past_temp 0.01 --distill_power 1.0
-
-# cifar100
-python main.py --method co2l --mem_type ring --dataset cifar100 --batch_size 512 --epochs 100 --start_epoch 500 \
-               --current_temp 0.2 --past_temp 0.01 --distill_power 1.0
